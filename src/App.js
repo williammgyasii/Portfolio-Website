6c52362a8566b3dc021978d4943383ef068b3d90
William Gyasi <gyasi.wk@gmail.com>
2024-08-29 13:27:57 +0000

    Paper Texture Look

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App font-sans text-lg">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        sans: ["Poppins", "sans-serif"],
+        mono: ["Fira Code", "monospace"],
+      },
+    },
   },
   plugins: [],
-}
+};
