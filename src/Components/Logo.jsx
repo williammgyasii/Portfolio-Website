8ff4aab0e0560df78739f61eddd2477a7806efd9
William Gyasi <gyasi.wk@gmail.com>
2024-08-29 14:29:25 +0000

    Adding React Router Dom

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -1,15 +1,30 @@
 import React from "react";
+import { motion } from "framer-motion";
+
+// const motionLink=motion(a)
+
 
 const Logo = () => {
   return (
     <div className="flex items-center justify-center mt-2">
-      <a
+      <motion.a
+        whileHover={{
+          transition: { duration: 1, repeat: Infinity },
+          backgroundColor: [
+            "#121212",
+            "rgba(131,58,180,1)",
+            "rgba(253,29,29,1)",
+            "rgba(252,176,69,1)",
+            "rgba(131,58,180,1)",
+            "#121212",
+          ],
+        }}
         href="/"
         className="w-12 h-12 bg-dark text-light flex items-center justify-center
       rounded-full text-lg font-bold font-sans"
       >
         WG<span className="text-green-700">.</span>
-      </a>
+      </motion.a>
     </div>
   );
 };
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,24 +1,39 @@
 import React from "react";
 import Logo from "./Logo";
+import { motion } from "framer-motion";
+
+const CustomLink = ({ href, title, className = "" }) => {
+  return (
+    <a href={href} className={`${className} relative group text-sm`}>
+      {title}
+      <span
+        className="h-[1px] inline-block w-0 bg-yellow-400 absolute left-0 -bottom-0.5
+      group-hover:w-full transition-[width] ease duration-300"
+      >
+        &nbsp;
+      </span>
+    </a>
+  );
+};
 
 const Navbar = () => {
   return (
     <header className="w-full px-8 font-normal flex items-center justify-between">
       <Logo />
       <nav>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/projects">Projects</a>
-        <a href="/articles">Articles</a>
+        <CustomLink href="/" title={"Home"} className="mr-4" />
+        <CustomLink href="/about" title={"About"} className="mx-4" />
+        <CustomLink href="/projects" title={"Projects"} className="mx-4" />
+        <CustomLink href="/articles" title={"Articles"} className="ml-4" />
       </nav>
 
       <nav>
         <a href="/" target="_blank">
           Twitter
         </a>
-        <a href="/" target="_blank">
+        <s href="/" target="_blank">
           Twitter
-        </a>
+        </s>
         <a href="/" target="_blank">
           Twitter
         </a>
