24a18e37071ced7500257c285aac70db9463fcee
William Gyasi <gyasi.wk@gmail.com>
2024-09-01 09:27:11 +0000

    Animated Code

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,22 +3,20 @@ import React from "react";
 
 const HeroSection = () => {
   return (
-    <section className=" min-h-screen w-full p-32 bg-purple-900 text-white ">
-      <div className="flex-1 flex flex-col items-start justify-center p-8">
-        <h1 className="text-4xl md:text-6xl font-bold mb-4">
-          Welcome to My Portfolio
-        </h1>
-        <p className="text-lg md:text-xl mb-6">
-          I am a passionate Front-End Developer with a knack for creating
-          beautiful and functional web applications.
-        </p>
-        <a
-          href="#projects"
-          className="bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded transition duration-300 ease-in-out"
-        >
-          See My Work
-        </a>
-      </div>
+    <section className=" flex items-center justify-between w-full">
+      <h1 className="text-4xl md:text-6xl font-bold mb-4">
+        Welcome to My Portfolio
+      </h1>
+      <p className="text-lg md:text-xl mb-6">
+        I am a passionate Front-End Developer with a knack for creating
+        beautiful and functional web applications.
+      </p>
+      <a
+        href="#projects"
+        className="bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded transition duration-300 ease-in-out"
+      >
+        See My Work
+      </a>
     </section>
   );
 };
--- src/Layouts/Layout.jsx
+++ src/Layouts/Layout.jsx
@@ -2,9 +2,7 @@ import React from "react";
 
 const Layout = ({ children, className }) => {
   return (
-    <div
-      className={`w-full w-screen h-full min-h-screen flex items-center block bg-primaryDark p-20 ${className}`}
-    >
+    <div className={`w-full h-full inline-block bg-dark p-32 ${className}`}>
       {children}
     </div>
   );
