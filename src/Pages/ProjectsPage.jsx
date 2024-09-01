a81e0133693daf054f8f313e44e6638369aa120d
William Gyasi <gyasi.wk@gmail.com>
2024-09-01 15:19:38 +0000

    Projects Page Fix

--- src/Pages/ProjectsPage.jsx
+++ src/Pages/ProjectsPage.jsx
@@ -3,13 +3,27 @@ import StarryBackgroundNoHover from "../AnimatedComponents/StarryBackground";
 import { useMotionTemplate, useMotionValue } from "framer-motion";
 import { motion } from "framer-motion";
 import Navbar from "../Components/Navbar";
+import ProjectHeroSection from "../Components/ProjectIntro";
 
 const COLORS_TOP = ["#13FFAA", "#1E67C6", "#CE84CF", "#DD335C"];
 
 const ProjectsPage = () => {
   const color = useMotionValue(COLORS_TOP[0]);
   const backgroundImage = useMotionTemplate`radial-gradient(125% 125% at 50% 0%, #020617 50%, ${color})`;
-  return <></>;
+  return (
+    <section className="bg-red-500 w-full flex flex-col items-center justify-center">
+      <Navbar />
+      <motion.main
+        style={{
+          backgroundImage,
+        }}
+        className=" flex flex-col bg-green-700  text-white w-full  items-center min-h-screen "
+      >
+        <ProjectHeroSection />
+      </motion.main>
+      <StarryBackgroundNoHover />
+    </section>
+  );
 };
 
 export default ProjectsPage;
