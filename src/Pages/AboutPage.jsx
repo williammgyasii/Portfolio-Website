f8dcdbfeb77377298afc4d398b44b5cc83342970
William Gyasi <gyasi.wk@gmail.com>
2024-09-01 14:42:47 +0000

    Hero

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import StarryBackgroundNoHover from "../AnimatedComponents/StarryBackground";
+import AboutHeroSection from "../Components/AboutHeroSection";
 import { useMotionTemplate, useMotionValue } from "framer-motion";
 import { motion } from "framer-motion";
 import Navbar from "../Components/Navbar";
@@ -25,25 +26,7 @@ const AboutPage = () => {
         }}
         className=" flex flex-col bg-green-700  text-white w-full  items-center min-h-screen "
       >
-        <motion.section
-          style={{ zIndex: 10 }}
-          className="items-center justify-center mt-xsHuge"
-        >
-          <motion.h1
-            style={{
-              borderBottom,
-            }}
-            whileHover={{
-              scale: 1.015,
-            }}
-            whileTap={{
-              scale: 0.985,
-            }}
-            className="inline-block text-4xl items-center cursor-pointer xs:text-lg rounded-md bg-gray-950/10 px-4 py-3 text-gray-50 transition-colors hover:bg-gray-950/50"
-          >
-            This is William Gyasi
-          </motion.h1>
-        </motion.section>
+        <AboutHeroSection />
       </motion.main>
 
       <StarryBackgroundNoHover />
