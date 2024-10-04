42ff9d0a77a7624f6730eef291432755ef715c05
William Gyasi <gyasi.wk@gmail.com>
2024-10-04 23:46:37 +0000

    Functions Not Working

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,45 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-const {onRequest} = require("firebase-functions/v2/https");
+const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+const functions = require("firebase-functions");
+const nodemailer = require("nodemailer");
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Configure your email and app password here (use environment variables in production)
+const gmailEmail = functions.config().gmail.email;
+const gmailPassword = functions.config().gmail.password;
+
+// Create a transporter using Gmail SMTP
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: gmailEmail,
+    pass: gmailPassword,
+  },
+});
+
+// Cloud Function to send an email
+exports.sendContactEmail = functions.https.onCall((data, context) => {
+  const { name, email, message } = data;
+
+  const mailOptions = {
+    from: gmailEmail,
+    to: gmailEmail, // You can send it to yourself
+    subject: `Contact Form Submission from ${name}`,
+    html: `
+      <p><b>Name:</b> ${name}</p>
+      <p><b>Email:</b> ${email}</p>
+      <p><b>Message:</b></p>
+      <p>${message}</p>
+    `,
+  };
+
+  return transporter
+    .sendMail(mailOptions)
+    .then(() => {
+      return { success: true };
+    })
+    .catch((error) => {
+      console.error("There was an error while sending the email:", error);
+      return { success: false, error: error.toString() };
+    });
+});
